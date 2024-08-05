0b537b4211edf37fc8d2787902383d6ddeb7ba39
weakbox <connormcleod2000@icloud.com>
2024-08-05 19:19:13 -0300

    Project passes required tests.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,76 @@
 import { useState } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
-const defaultMarkdown = `# Welcome to my React Markdown Previewer!`;
+const defaultInput: string = `# Welcome to my React Markdown Previewer!
 
-const convertMarkdown = (text: string) => {
-  return `MARKDOWN: ` + text;
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
+const convertMarkdown = (text: string): any => {
+  return marked.parse(text);
 };
 
 function App() {
-  const [text, setText] = useState(convertMarkdown(defaultMarkdown));
+  const [text, setText] = useState(convertMarkdown(defaultInput));
 
   const handleChange = (event: any) => {
-    setText(convertMarkdown(event.target.value));
+    setText(event.target.value);
   };
 
   return (
     <div className="background">
-      <h1>Markdown Previewer</h1>
+      <h1>Simple Markdown Previewer</h1>
       <div className="markdown-container">
         <textarea 
           id="editor"
           onChange={handleChange}
-          defaultValue={defaultMarkdown}
+          defaultValue={defaultInput}
         ></textarea>
-        <div id="preview">{text}</div>
+        <div 
+          id="preview"
+          dangerouslySetInnerHTML={ {__html: convertMarkdown(text)} }
+        ></div>
       </div>
     </div>
   );
